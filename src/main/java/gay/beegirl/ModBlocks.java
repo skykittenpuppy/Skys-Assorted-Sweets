9f815564816b12894f2bb0612f2992e837d5731f
Hannah Hadfield <robotooth13alternative@gmail.com>
2024-06-30 13:07:29 +0100

    Oh lawd have mercy theres some shitty textures

--- src/main/java/gay/beegirl/ModBlocks.java
+++ src/main/java/gay/beegirl/ModBlocks.java
@@ -9,6 +9,7 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
 
 public class ModBlocks {
     public static void registerModBlocks(){
@@ -24,7 +25,7 @@ public class ModBlocks {
 
     public static final Block DEBUG = registerBlock("debug", new Block(AbstractBlock.Settings.copy(Blocks.STONE)));
 
-    public static final Block CAKE = registerBlock("cake", new PlaceableFoodBlock(
+    public static final Block SPONGE_CAKE = registerBlock("sponge_cake", new PlaceableFoodBlock(
             AbstractBlock.Settings.copy(Blocks.CAKE),
             7,
             new VoxelShape[]{
@@ -37,13 +38,65 @@ public class ModBlocks {
                     Block.createCuboidShape(01.0, 00.0, 01.0, 15.0, 08.0, 15.0),
             }
     ));
-    public static final Block TART = registerBlock("tart", new PlaceableFoodBlock(
+    public static final Block CHOCOLATE_CAKE = registerBlock("chocolate_cake", new PlaceableFoodBlock(
+            AbstractBlock.Settings.copy(Blocks.CAKE),
+            7,
+            new VoxelShape[]{
+                    Block.createCuboidShape(13.0, 00.0, 01.0, 15.0, 08.0, 15.0),
+                    Block.createCuboidShape(11.0, 00.0, 01.0, 15.0, 08.0, 15.0),
+                    Block.createCuboidShape(09.0, 00.0, 01.0, 15.0, 08.0, 15.0),
+                    Block.createCuboidShape(07.0, 00.0, 01.0, 15.0, 08.0, 15.0),
+                    Block.createCuboidShape(05.0, 00.0, 01.0, 15.0, 08.0, 15.0),
+                    Block.createCuboidShape(03.0, 00.0, 01.0, 15.0, 08.0, 15.0),
+                    Block.createCuboidShape(01.0, 00.0, 01.0, 15.0, 08.0, 15.0),
+            }
+    ));
+    public static final Block CARROT_CAKE = registerBlock("carrot_cake", new PlaceableFoodBlock(
+            AbstractBlock.Settings.copy(Blocks.CAKE),
+            7,
+            new VoxelShape[]{
+                    Block.createCuboidShape(13.0, 00.0, 01.0, 15.0, 08.0, 15.0),
+                    Block.createCuboidShape(11.0, 00.0, 01.0, 15.0, 08.0, 15.0),
+                    Block.createCuboidShape(09.0, 00.0, 01.0, 15.0, 08.0, 15.0),
+                    Block.createCuboidShape(07.0, 00.0, 01.0, 15.0, 08.0, 15.0),
+                    Block.createCuboidShape(05.0, 00.0, 01.0, 15.0, 08.0, 15.0),
+                    Block.createCuboidShape(03.0, 00.0, 01.0, 15.0, 08.0, 15.0),
+                    Block.createCuboidShape(01.0, 00.0, 01.0, 15.0, 08.0, 15.0),
+            }
+    ));
+    public static final Block PUMPKIN_PIE = registerBlock("pumpkin_pie", new PlaceableFoodBlock(
             AbstractBlock.Settings.copy(Blocks.CAKE),
             4,
             new VoxelShape[]{
-                    Block.createCuboidShape(10.0, 00.0, 04.0, 12.0, 04.0, 12.0),
+                    Block.createCuboidShape(08.0, 00.0, 08.0, 12.0, 04.0, 12.0),
                     Block.createCuboidShape(08.0, 00.0, 04.0, 12.0, 04.0, 12.0),
-                    Block.createCuboidShape(06.0, 00.0, 04.0, 12.0, 04.0, 12.0),
+                    VoxelShapes.union(
+                        Block.createCuboidShape(08.0, 00.0, 04.0, 12.0, 04.0, 12.0),
+                        Block.createCuboidShape(04.0, 00.0, 04.0, 12.0, 04.0, 8.0)),
+                    Block.createCuboidShape(04.0, 00.0, 04.0, 12.0, 04.0, 12.0),
+            }
+    ));
+    public static final Block APPLE_PIE = registerBlock("apple_pie", new PlaceableFoodBlock(
+            AbstractBlock.Settings.copy(Blocks.CAKE),
+            4,
+            new VoxelShape[]{
+                    Block.createCuboidShape(08.0, 00.0, 08.0, 12.0, 04.0, 12.0),
+                    Block.createCuboidShape(08.0, 00.0, 04.0, 12.0, 04.0, 12.0),
+                    VoxelShapes.union(
+                            Block.createCuboidShape(08.0, 00.0, 04.0, 12.0, 04.0, 12.0),
+                            Block.createCuboidShape(04.0, 00.0, 04.0, 12.0, 04.0, 8.0)),
+                    Block.createCuboidShape(04.0, 00.0, 04.0, 12.0, 04.0, 12.0),
+            }
+    ));
+    public static final Block BERRY_PIE = registerBlock("berry_pie", new PlaceableFoodBlock(
+            AbstractBlock.Settings.copy(Blocks.CAKE),
+            4,
+            new VoxelShape[]{
+                    Block.createCuboidShape(08.0, 00.0, 08.0, 12.0, 04.0, 12.0),
+                    Block.createCuboidShape(08.0, 00.0, 04.0, 12.0, 04.0, 12.0),
+                    VoxelShapes.union(
+                            Block.createCuboidShape(08.0, 00.0, 04.0, 12.0, 04.0, 12.0),
+                            Block.createCuboidShape(04.0, 00.0, 04.0, 12.0, 04.0, 8.0)),
                     Block.createCuboidShape(04.0, 00.0, 04.0, 12.0, 04.0, 12.0),
             }
     ));
@@ -56,6 +109,14 @@ public class ModBlocks {
                     Block.createCuboidShape(04.0, 00.0, 04.0, 12.0, 04.0, 12.0),
                     Block.createCuboidShape(04.0, 00.0, 04.0, 12.0, 05.0, 12.0),
                     Block.createCuboidShape(04.0, 00.0, 04.0, 12.0, 06.0, 12.0),
+            }
+    ));
+    public static final Block TREACLE_TART = registerBlock("treacle_tart", new PlaceableFoodBlock(
+            AbstractBlock.Settings.copy(Blocks.CAKE),
+            2,
+            new VoxelShape[]{
+                    Block.createCuboidShape(08.0, 00.0, 04.0, 12.0, 04.0, 12.0),
+                    Block.createCuboidShape(04.0, 00.0, 04.0, 12.0, 04.0, 12.0),
             }
     ));
 }
